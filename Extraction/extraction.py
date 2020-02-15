04b4ab8485d69a0cfee491257e443d5f3f38892d
Matheus Soutto <matheussoutto27@gmail.com>
2020-02-15 19:29:47 -0300

    configurando proxy do driver e lidando com alert

--- Extraction/extraction.py
+++ Extraction/extraction.py
@@ -1,30 +1,37 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path=r"C:\Users\MatheusdeAbreuGoncal\Downloads\chromedriver_win32\chromedriver.exe")
+proxyString = "spica.eic.cefet-rj.br:8085"
 
+desired_capability = webdriver.DesiredCapabilities.FIREFOX
+desired_capability['proxy'] = {
+    "proxyType": "manual",
+    "httpProxy": proxyString,
+    "ftpProxy": proxyString,
+    "sslProxy": proxyString
+}
+
+profile = webdriver.FirefoxProfile()
+profile.accept_untrusted_certs = True
+profile.set_preference('app.update.enabled', False)
+profile.set_preference('app.update.auto', False)
+profile.set_preference('app.update.service.enabled', False)
+#profile.set_preference('app.update.staging.enabled', False)
+#profile.set_preference('app.update.silent', False)
+profile.update_preferences()
+
+driver = webdriver.Firefox(firefox_profile=profile, capabilities=desired_capability, executable_path=r"C:\Users\MatheusdeAbreuGoncal\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe")
+
+alert = driver.switch_to.alert
+alert.send_keys("44878904895" + Keys.TAB + "44878904895")
+alert.accept()
+
+driver.switch_to.window(driver.window_handles[0])
 driver.maximize_window()
 
 driver.get("https://www.periodicos.capes.gov.br/")
 
 driver.find_element_by_xpath(".//button[text()='Fechar']").click()
-
-#################### SELECT INSTITUTION ######################
-
-driver.find_element_by_class_name("chosen-single").click()
-
-driver.find_element_by_xpath(".//div[@class='chosen-search']/input").send_keys('uff' + Keys.ENTER)
-
-driver.find_element_by_id("enviarInstituicaoCafe").click()
-
-#################### LOGIN FORM ######################
-
-driver.find_element_by_id("username").send_keys("01444990535")
-
-driver.find_element_by_id("password").send_keys("metodologia")
-
-driver.find_element_by_xpath(".//button[@type='submit']").click()
 
 
 #################### END ######################
